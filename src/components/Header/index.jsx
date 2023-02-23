f2876cce846023b71cbf3cd87b86068815938521
Bruno Netto De Souza Rosa <mir_brosa@uolinc.com>
2023-02-23 12:15:27 -0300

    feat(header component): adds second version of header, now responsive

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,33 +1,87 @@
+import { use, useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 import LogoWhite from "../../assets/logo-white.svg"
 
 export default function Header() {
-  return (
-    <header className="bg-black p-10 text-white text-xl">
-      <div className="container mx-auto flex justify-between">
-        <Image
-          src={LogoWhite}
-          alt="Logo do projeto"
-          width="auto"
-          height="auto"
-        />
+  const [navbar, setNavbar] =  useState(false);
 
-        <nav className="flex items-center">
-          <ul className="flex justify-between items-center gap-10">
-            <li>
-              <Link href="/">Home</Link>
-            </li>
-            <li>
-              <Link href="/about">Sobre</Link>
-            </li>
-            <li>
-              <Link href="/contact">Contato</Link>
-            </li>
-          </ul>
-        </nav>
-      </div>
+  return (
+    <header className="w-full bg-black">
+      <nav className="justify-between px-4 mx-auto md:items-center md:flex md:px-8 lg:max-w-7xl">
+        <div>
+          <div className="flex items-center justify-between py-3 md:py-5 md:block">
+            <Link href="/home">
+              <Image
+                src={LogoWhite}
+                alt="Logo do projeto"
+                width="auto"
+                height="auto"
+              />
+            </Link>
+
+            <div className="md:hidden">
+              <button
+                className="p-2 text-white"
+                onClick={() => setNavbar(!navbar)}
+              >
+                {navbar ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+        </div>
+
+        <div>
+          <div
+            className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
+              navbar ? "block" : "hidden"
+            }`}
+          >
+            <ul className="items-center justify-center space-y-8 md:flex md:space-x-14 md:space-y-0">
+              <li className="text-xl text-white hover:underline">
+                <Link href="/">Home</Link>
+              </li>
+
+              <li className="text-xl text-white hover:underline">
+                <Link href="/about">Sobre</Link>
+              </li>
+
+              <li className="text-xl text-white hover:underline">
+                <Link href="/contact">Contato</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     </header>
-  )
+  );
 }
